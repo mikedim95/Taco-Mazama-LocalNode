babf4087bf5d4847bd371383a89af4077446fd7b
Pappantelis <pappantelispap@gmail.com>
2023-09-23 21:42:16 +0300

    dish.size

--- utils/buildOrderForPrinter.js
+++ utils/buildOrderForPrinter.js
@@ -16,6 +16,7 @@ const buildOrderForPrinter = (orderMessage) => {
     formattedOrder += "PIATA:\n-------\n";
     for (const dish of order.dish) {
       formattedOrder += `${dish.multiplier}x\n`;
+      formattedOrder += `${dish.size}`;
       formattedOrder += `${greekUtils.toGreeklish(dish.title).toUpperCase()}\n`;
       if (dish.stuffing)
         formattedOrder += `${greekUtils.toGreeklish(
